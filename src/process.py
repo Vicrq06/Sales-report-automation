0bcf9d123402f6a893bf005f748e68f71974ab17
Victor <vicrq06@gmail.com>
2026-04-27 18:54:45 -0500

    Cambios en process y mejora de manejo de errores

--- src/process.py
+++ src/process.py
@@ -29,40 +29,63 @@ def combinar_data(files):
     log.debug("Dataframe combinado.")
     return df
 
-def exportar(df):
-    df.to_excel(Output_dir/"df_final",index=True)
-    log.debug("Se ha exportado el dataframe")
+def exportar(df,to=None):
+    try:
+        if to=="Excel":
+            df.to_excel(Output_dir/"df_final",index=True)
+            log.debug("Se ha exportado el excel del dataframe")
+        elif to=="csv":
+            df.to_csv(Output_dir/"df_final")
+            log.debug("Se ha exportado el csv del dataframe")
+    except Exception as e:
+        log.error(f"Ha ocurrido un error al exportar a {to}: ", e)
 
 def estadisticas_pd(df):
     return df.describe()
 
 def merge(df_i,df_d,how):
-    df_merge=pd.merge(df_i,df_d,how=how)
-    log.debug("Se ha hecho merge del dataframe")
-    return df_merge
+    try:
+        df_merge=pd.merge(df_i,df_d,how=how)
+        log.debug("Se ha hecho merge del dataframe")
+        return df_merge
+    except Exception as e:
+        log.error(f"Error al agrupar el dataframe: {e}")
+        return None
 
 def pivot_pd(df,indice,columna,valores):
-    df1=df.pivot(index=indice,columns=columna,values=valores).copy()
-    log.debug("Se ha hecho pivot del dataframe")
-    return df1
+    try:
+        df1=df.pivot(index=indice,columns=columna,values=valores).copy()
+        log.debug("Se ha hecho pivot del dataframe")
+        return df1
+    except Exception as e:
+        log.error(f"Error al agrupar el dataframe: {e}")
+        return None
+
 
 def pivot_table_pd(df,indice,columna,valores,funcion,marg:bool):
-    df_pivot=df.pivot_table(index=indice,columns=columna,values=valores,aggfunc=funcion,margins=marg).copy()
-    log.debug("Se ha hecho pivot_table del dataframe")
-    return df_pivot
+    try:
+        df_pivot=df.pivot_table(index=indice,columns=columna,values=valores,aggfunc=funcion,margins=marg).copy()
+        log.debug("Se ha hecho pivot_table del dataframe")
+        return df_pivot
+    except Exception as e:
+        log.error(f"Error al agrupar el dataframe: {e}")
+        return None
 
 def group_by(df:pd.DataFrame,
              columnas_a_agrupar:list[str],
              agregaciones):
-    
-    log.debug("Se ha hecho groupby del dataframe")
-    resultado = df.groupby(columnas_a_agrupar).agg(agregaciones).copy()
-
-    return resultado
+    try:
+        log.debug("Se ha hecho groupby del dataframe")
+        resultado = df.groupby(columnas_a_agrupar).agg(agregaciones).copy()
+        return resultado
+    except Exception as e:
+        log.error(f"Error al agrupar el dataframe: {e}")
+        return None
 
 def export_graph(df,tipo):
     df.plot(kind=tipo)
     plt.title("Producto vs precio")
     plt.ylabel("Precio")
     plt.show()
+    
 
