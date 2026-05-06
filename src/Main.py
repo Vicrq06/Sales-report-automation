e03211f3158ac5156933472142888ce3cee39e0f
Victor <vicrq06@gmail.com>
2026-05-06 11:12:57 -0500

    Cambios realizados en main.py

--- src/Main.py
+++ src/Main.py
@@ -11,17 +11,26 @@ if __name__=="__main__":
     df=combinar_data(archivos)
     df_estadisticas=process.estadisticas_pd(df)
     
+    print ("Df combinado")
     print (df)
-    print ("PIVOT")
 
+
+    print ("Ejecutando analisis de estadisticas básicas (describe)")
+    print (df_estadisticas)
+
+    print ("Cambiando formato largo a ancho (pivot)")
     df_pivot=process.pivot_pd(df,indice="Fecha",columna="Id_producto",valores="Total")
     print (df_pivot)
 
+    print ("Agrupando por id y usando suma como metodo de agregacion:")
     df_groupby=process.group_by(df,("Id_producto"),{"Total":"sum"})
     print (df_groupby)
 
-    df_pivot_table=process.pivot_table_pd(df,("Id_producto"),"Producto","Total",sum,False).fillna(0)
+    print ("Pivot table usando suma como método de agregacion sin margins:")
+    df_pivot_table=process.pivot_table_pd(df,"Producto",("Id_producto"),"Total",sum,False).fillna(0)
     print (df_pivot_table)
+
+    print ("Exportando gráfica de pivot table")
     process.export_graph(df_pivot_table,"bar")
 
 
